43c900296220e8abf85c768f0a432b067ad119f3
Eduardo Fereda <feredaeduardo@gmail.com>
2025-08-30 01:20:17 +0000

    avances en el login

--- src/app.py
+++ src/app.py
@@ -9,6 +9,7 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from api.routes import bcrypt
+from flask_jwt_extended import create_access_token, JWTManager
 
 # from models import Person
 
@@ -31,6 +32,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 app.config["SECRET_KEY"]= os.getenv("SECRET_KEY")
+jwt = JWTManager(app)
 
 # add the admin
 setup_admin(app)
